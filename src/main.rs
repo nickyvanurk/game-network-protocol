6b37df1abd74cf1ba8102964b69407191d87ad58
Nicky van Urk <nickyvanurk@gmx.com>
2022-01-20 23:12:18 +0100

    Add read/write int functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = Buffer {
+        data: &mut [0; 100][0],
+        size: 100,
+        index: 0,
+    };
+
+    write_integer(&mut buffer, 42);
+
+    buffer.index = 0;
+
+    println!("{:?}", read_integer(&mut buffer));
+}
+
+struct Buffer {
+    data: *mut u8,
+    size: usize,
+    index: usize,
+}
+
+fn write_integer(buffer: &mut Buffer, value: u32) {
+    assert!(buffer.index + 4 <= buffer.size);
+    unsafe {
+        *(buffer.data.add(buffer.index) as *mut u32) = value;
+    }
+    buffer.index += 4;
+}
+
+fn read_integer(buffer: &mut Buffer) -> u32 {
+    assert!(buffer.index + 4 <= buffer.size);
+    let value = unsafe {
+        *(buffer.data.add(buffer.index) as *const u32)
+    };
+    return value;
 }

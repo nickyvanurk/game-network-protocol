f7788ad6756403babd1bd89f8e458ade760001db
Nicky van Urk <nickyvanurk@gmx.com>
2022-01-21 00:23:06 +0100

    Add packet C read/write

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,20 @@ fn main() {
     println!("{:?}", packet_b);
 
     assert_eq!(packet_a, packet_b);
+
+    //---------------Packet C---------------
+    let mut buffer = Buffer::new(100);
+    let mut packet_a = PacketC { x: true, y: 7, z: 13 };
+    packet_a.write(&mut buffer);
+    println!("{:?}", packet_a);
+
+    buffer.index = 0;
+
+    let mut packet_b = PacketC { x: false, y: 0, z: 0 };
+    packet_b.read(&mut buffer);
+    println!("{:?}", packet_b);
+
+    assert_eq!(packet_a, packet_b);
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -73,6 +87,27 @@ impl PacketB {
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
+struct PacketC {
+    x: bool,
+    y: u16,
+    z: u32,
+}
+
+impl PacketC {
+    fn write(&mut self, buffer: &mut Buffer) {
+        write_byte(buffer, self.x as u8);
+        write_short(buffer, self.y);
+        write_integer(buffer, self.z);
+    }
+
+    fn read(&mut self, buffer: &mut Buffer) {
+        self.x = read_byte(buffer) == 1;
+        self.y = read_short(buffer);
+        self.z = read_integer(buffer);
+    }
+}
+
 struct Buffer {
     data: Vec<u8>,
     size: usize,
@@ -103,7 +138,7 @@ fn write_short(buffer: &mut Buffer, value: u16) {
 
 fn write_byte(buffer: &mut Buffer, value: u8) {
     assert!(buffer.index <= buffer.size);
-    buffer.data[buffer.index+1] = value;
+    buffer.data[buffer.index] = value;
     buffer.index += 1;
 }
 
@@ -121,5 +156,6 @@ fn read_short(buffer: &mut Buffer) -> u16 {
 
 fn read_byte(buffer: &mut Buffer) -> u8 {
     assert!(buffer.index <= buffer.size);
-    buffer.data[buffer.index+1]
+    buffer.index += 1;
+    buffer.data[buffer.index-1]
 }

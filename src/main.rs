0302b99c2d08985f390ed0bc056dce3a0f5eb816
Nicky van Urk <nickyvanurk@gmx.com>
2022-01-23 13:24:59 +0100

    Add packet wrapper

--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,79 @@ use byteorder::{ByteOrder, LittleEndian};
 fn main() {
     //---------------Packet A---------------
     let mut buffer = Buffer::new(100);
-    let mut packet_a = PacketA { x: 10, y: 15, z: 20 };
-    packet_a.write(&mut buffer);
-    println!("{:?}", packet_a);
-
+    let packet = Packet::A(PacketA { x: 10, y: 15, z: 20 });
+    packet.write(&mut buffer);
+    println!("{:?}", packet);
     buffer.index = 0;
-    
-    let mut packet_b = PacketA { x: 0, y: 0, z: 0 };
-    packet_b.read(&mut buffer);
-    println!("{:?}", packet_b);
-
-    assert_eq!(packet_a, packet_b);
-
+    println!("{:?}\n", Packet::new(&mut buffer));
     //---------------Packet B---------------
     let mut buffer = Buffer::new(100);
-    let mut packet_a = PacketB { elements: vec![1, 2, 3, 4, 5], num_elements: 5 };
-    packet_a.write(&mut buffer);
-    println!("{:?}", packet_a);
-
+    let packet = Packet::B(PacketB { elements: vec![1, 2, 3, 4, 5], num_elements: 5 });
+    packet.write(&mut buffer);
+    println!("{:?}", packet);
     buffer.index = 0;
-    
-    let mut packet_b = PacketB { num_elements: 0, elements: vec![] };
-    packet_b.read(&mut buffer);
-    println!("{:?}", packet_b);
-
-    assert_eq!(packet_a, packet_b);
-
+    println!("{:?}\n", Packet::new(&mut buffer));
     //---------------Packet C---------------
     let mut buffer = Buffer::new(100);
-    let mut packet_a = PacketC { x: true, y: 7, z: 13 };
-    packet_a.write(&mut buffer);
-    println!("{:?}", packet_a);
-
+    let packet = Packet::C(PacketC { x: true, y: 7, z: 13 });
+    packet.write(&mut buffer);
+    println!("{:?}", packet);
     buffer.index = 0;
-
-    let mut packet_b = PacketC { x: false, y: 0, z: 0 };
-    packet_b.read(&mut buffer);
-    println!("{:?}", packet_b);
-
-    assert_eq!(packet_a, packet_b);
+    println!("{:?}", Packet::new(&mut buffer));
 }
 
-#[derive(Debug, Eq, PartialEq)]
+// #[derive(Debug)]
+enum PacketType { PacketA, PacketB, PacketC }
+
+#[derive(Debug)]
+enum Packet {
+    A(PacketA),
+    B(PacketB),
+    C(PacketC),
+}
+
+impl Packet {
+    fn write(&self, buffer: &mut Buffer) {
+        match self {
+            Packet::A(packet) => {
+                write_byte(buffer, PacketType::PacketA as u8);
+                packet.write(buffer);
+            },
+            Packet::B(packet) => {
+                write_byte(buffer, PacketType::PacketB as u8);
+                packet.write(buffer)
+            },
+            Packet::C(packet) => {
+                write_byte(buffer, PacketType::PacketC as u8);
+                packet.write(buffer)
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    fn read(&mut self, buffer: &mut Buffer) {
+        match self {
+            Packet::A(packet) => packet.read(buffer),
+            Packet::B(packet) => packet.read(buffer),
+            Packet::C(packet) => packet.read(buffer),
+            _ => unreachable!(),
+        };
+    }
+
+    fn new(buffer: &mut Buffer) -> Self {
+        let packet_type = read_byte(buffer);
+        let mut packet = match packet_type {
+            x if x == PacketType::PacketA as u8 => Packet::A(PacketA { x: 0, y: 0, z: 0}),
+            x if x == PacketType::PacketB as u8 => Packet::B(PacketB { num_elements: 0, elements: vec![] }),
+            x if x == PacketType::PacketC as u8 => Packet::C(PacketC { x: false, y: 0, z: 0 }),
+            _ => unreachable!(),
+        };
+        packet.read(buffer);
+        packet
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct PacketA {
     x: u32,
     y: u32,
@@ -52,7 +83,7 @@ struct PacketA {
 }
 
 impl PacketA {
-    fn write(&mut self, buffer: &mut Buffer) {
+    fn write(&self, buffer: &mut Buffer) {
         write_int(buffer, self.x);
         write_int(buffer, self.y);
         write_int(buffer, self.z);
@@ -65,14 +96,14 @@ impl PacketA {
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 struct PacketB {
     num_elements: u32,
     elements: Vec<u32>,
 }
 
 impl PacketB {
-    fn write(&mut self, buffer: &mut Buffer) {
+    fn write(&self, buffer: &mut Buffer) {
         write_int(buffer, self.num_elements);
         for i in 0..self.num_elements {
             write_int(buffer, self.elements[i as usize]);
@@ -87,7 +118,7 @@ impl PacketB {
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct PacketC {
     x: bool,
     y: u16,
@@ -95,7 +126,7 @@ struct PacketC {
 }
 
 impl PacketC {
-    fn write(&mut self, buffer: &mut Buffer) {
+    fn write(&self, buffer: &mut Buffer) {
         write_byte(buffer, self.x as u8);
         write_short(buffer, self.y);
         write_int(buffer, self.z);
@@ -108,6 +139,7 @@ impl PacketC {
     }
 }
 
+#[derive(Debug)]
 struct Buffer {
     data: Vec<u8>,
     size: usize,
